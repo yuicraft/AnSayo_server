f6e0978ae2005054057f36b0e745792c34fc4d34
yuicraft <twins20131988@gmail.com>
2019-06-26 01:17:12 +0900

    Add subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post '/get_data', to: 'wiki#getting_data'
-  post '/patch_data', to: 'wiki#patching_data'
+  post '/get_data', to: 'wiki#getting_data', constraints: { subdomain: 'ansayo' }
+  post '/patch_data', to: 'wiki#patching_data', constraints: { subdomain: 'ansayo' }
 end
