5aef0b172c1935a9851e7f2f33eb14ec2a675d07
yuicraft <twins20131988@gmail.com>
2019-06-26 00:56:16 +0900

    Add patching_data, Add gettin_data

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -1,2 +1,25 @@
 class WikiController < ApplicationController
+ def getting_data
+  if Content.exists?(title: params[:title])
+	cont = Content.find_by(title: params[:title])
+	render json: {
+	  "title": cont.title,
+	  "context": cont.content
+	}
+  
+  else
+	head :bad_request
+  end
+ end
+ def patching_data
+  if Content.exists?(title: params[:title])
+	cont = Content.find_by(title: params[:title])
+ 	cont.content = params[:content]
+	head :ok if cont.save
+	
+  else
+	Content.create(title: params[:title],content: params[:content])
+	head :ok
+  end
+ end
 end
