5add0c3379d6100f36d2c896102320ab554146ef
yuicraft <twins20131988@gmail.com>
2019-06-26 01:00:54 +0900

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/get_data', to: 'wiki#gettching_data'
+  post '/patch_data', to: 'wiki#patching_data'
 end
