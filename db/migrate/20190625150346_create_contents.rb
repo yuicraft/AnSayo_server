7780bc8f4e10ede1481158d890010722bd690638
yuicraft <twins20131988@gmail.com>
2019-06-26 00:30:00 +0900

    add db

--- db/migrate/20190625150346_create_contents.rb
+++ db/migrate/20190625150346_create_contents.rb
@@ -1,7 +1,8 @@
 class CreateContents < ActiveRecord::Migration[5.2]
   def change
     create_table :contents do |t|
-
+      t.string :title, null: false , default: ""
+      t.string :content, null: false, default: ""
       t.timestamps
     end
   end
